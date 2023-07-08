401bf5a2479eff948606fee0787e3c906eecd1e2
smiurgh9 <simurgh9@pm.me>
2023-07-08 01:30:32 -0500

    tried the subsetsum experiment with 100 element sets.

--- code/_experiment/subsetsum.py
+++ code/_experiment/subsetsum.py
@@ -1,38 +1,32 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 
-RND = np.random.default_rng(seed=3)
+RND = np.random.default_rng(seed=0)
 
-set_cardinality = 30
+set_cardinality = 100
 examples = 2**10
 upper_bound = 2**8
 testing_ratio = 0.33
-
+index_to_question = 0  # whether the 0th element of M is in the sum
 M = RND.integers(upper_bound, size=set_cardinality, dtype=np.ulonglong)
 X = RND.integers(2, size=(set_cardinality, examples), dtype=M.dtype)
-X[0][::2], X[0][1::2] = 0, 1
+X[index_to_question][::2], X[index_to_question][1::2] = 0, 1
 S = M @ X
 
 data = np.hstack((np.repeat([M], S.shape[0], axis=0), S.reshape(-1, 1)))
 labels = X[0]
 
-threshold = labels.shape[0] - int(testing_ratio * labels.shape[0])
-train_X = data[:threshold]
-train_y = labels[:threshold]
-test_X = data[threshold:]
-test_y = labels[threshold:]
+at = labels.shape[0] - int(testing_ratio * labels.shape[0])
+X, y, _X, _y = data[:at], labels[:at], data[at:], labels[at:]
 
-best = 0
-for n in range(1, 101):
-    network = MLPClassifier(random_state=0,
-                            verbose=False,
-                            max_iter=2**32,
-                            hidden_layer_sizes=n)
-    network.fit(train_X, train_y)
-    pred_y = network.predict(test_X)
-    cur = np.sum(test_y == pred_y) / test_y.shape[0]
+best, accumulate = 0, 0
+for n in range(1, 5 * set_cardinality):
+    network = MLPClassifier(n, random_state=0, verbose=False, max_iter=2**32)
+    network.fit(X, y)
+    cur = np.sum(network.predict(_X) == _y) / _y.shape[0]
+    accumulate += cur
     if (cur > best):
         best = cur
-        print(f'{n:<3d} hidden units with accuracy {best:3f}')
+        print(f'{n:<3d} hidden units, accuracy: {best:3f}')
     else:
-        print(f'{n:<3d}', end='\r')
+        print(f'{n:<3d} hidden units, average: {accumulate / n:3f}', end='\r')
