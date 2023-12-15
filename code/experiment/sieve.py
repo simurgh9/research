ac2c93232b032c22f148b9fec4a39b3dd23614e1
smiurgh9 <simurgh9@pm.me>
2023-12-15 17:49:51 -0600

    increased initial population size by 10 folds.

--- code/experiment/sieve.py
+++ code/experiment/sieve.py
@@ -4,7 +4,7 @@ from lib.lattice import sieve, population
 # B = np.array([[3, 0], [0, 7]])
 # B = np.array([[95, 47], [460, 215]])
 B = np.loadtxt('columns.csv', delimiter=',')
-dims = {2: 4, 40: 650, 50: 700, 60:9000, 70:100000, 100:100000}
+dims = {2: 4, 40: 650, 50: 700, 60:9000, 70:100000, 100:1000000}
 size =  dims[100]
 
 P = population(B, size)
